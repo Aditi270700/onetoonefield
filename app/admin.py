3a6b30ceb42a4d5f42cd04689ae09e958b201458
aditi saudagar <aditisaudagar17@gmail.com>
2025-01-21 15:11:56 +0530

    onetomany

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Aadhar
-from .models import userProfile
+# from .models import Aadhar
+# from .models import userProfile
 
 
-admin.site.register(Aadhar)
-admin.site.register(userProfile)
+# admin.site.register(Aadhar)
+# admin.site.register(userProfile)
+
+from .models import Departmant
+from .models import Student
+
+admin.site.register(Departmant)
+admin.site.register(Student)
--- app/models.py
+++ app/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
+# one to one field
+# class Aadhar(models.Model):
+#    aadhar_no = models.IntegerField(unique=True)
+#    created_by = models.CharField(max_length=50)
+#    created_data = models.DateField()
+#    def __str__(self):
+#       return str(self.aadhar_no)
 
-class Aadhar(models.Model):
-   aadhar_no = models.IntegerField(unique=True)
-   created_by = models.CharField(max_length=50)
-   created_data = models.DateField()
-   def __str__(self):
-      return str(self.aadhar_no)
+# class userProfile(models.Model):
+#    name = models.CharField(max_length=50)
+#    email = models.EmailField()
+#    contact = models.IntegerField()
+#    aadhar = models.OneToOneField(Aadhar,on_delete=models.PROTECT)
 
-class userProfile(models.Model):
-   name = models.CharField(max_length=50)
-   email = models.EmailField()
-   contact = models.IntegerField()
-   aadhar = models.OneToOneField(Aadhar,on_delete=models.PROTECT)
+class Departmant(models.Model):
+   dep_name = models.CharField(max_length=50)
+   dep_des = models.TextField()
+   dep_hod = models.CharField(max_length=50)
+
+class Student(models.Model):
+   student_name = models.CharField(max_length=50)
+   student_email = models.EmailField()
+   student_dep = models.CharField(max_length=50)
+   student_roll = models.IntegerField()
