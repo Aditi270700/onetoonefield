e8443b476b6eda808eef137376c1e78266ccb850
aditi saudagar <aditisaudagar17@gmail.com>
2025-01-23 15:20:45 +0530

    manytomany

--- app/admin.py
+++ app/admin.py
@@ -7,9 +7,16 @@ from django.contrib import admin
 
 # admin.site.register(Aadhar)
 # admin.site.register(userProfile)
-
+'''
+one to many field
 from .models import Departmant
 from .models import Student
 
 admin.site.register(Departmant)
 admin.site.register(Student)
+'''
+from .models import Vehicle
+from .models import Fuel
+
+admin.site.register(Vehicle)
+admin.site.register(Fuel)
--- app/models.py
+++ app/models.py
@@ -13,9 +13,9 @@ from django.db import models
 #    email = models.EmailField()
 #    contact = models.IntegerField()
 #    aadhar = models.OneToOneField(Aadhar,on_delete=models.PROTECT)
-
+'''
 class Departmant(models.Model):
-   dep_name = models.CharField(max_length=50)
+   dep_name = models.CharField(max_length=50,unique=True)
    dep_des = models.TextField()
    dep_hod = models.CharField(max_length=50)
    def __str__(self):
@@ -24,7 +24,22 @@ class Departmant(models.Model):
 class Student(models.Model):
    student_name = models.CharField(max_length=50)
    student_email = models.EmailField()
-   student_dep = models.CharField(max_length=50)
+   student_dep = models.ForeignKey(Departmant,on_delete=models.PROTECT,to_field="dep_name")
    student_roll = models.IntegerField()
    def __str__(self):
       return self.student_name
+'''
+# many to many
+v_nm=[('Tata','Tata'),('BMW','BMW'),('Audi','Audi')]
+f_nm = [('Petrol','petrol'),('Desel','Desel'),('CNG','CNG'),('EV','EV')]
+class Vehicle(models.Model):
+   v_name = models.CharField(max_length=50,choices=v_nm)
+   def __str__(self):
+      return self.v_name
+class Fuel(models.Model):
+   f_name=models.CharField(max_length=50,choices=f_nm)
+   v_name=models.ManyToManyField(Vehicle)
+   def __str__(self):
+      return self.f_name
+
+      
